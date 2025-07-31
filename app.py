21a38d9142c312387518c5c506f3de5fba3add68
Kanwaljeet <kawal20905@gmail.com>
2025-07-31 16:54:53 +0530

    new-changes

--- app.py
+++ app.py
@@ -61,9 +61,12 @@ def delete_task(task_id):
     conn.close()
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 import schedule
 import time
